29943c6e886c7634ada4566286b83ceb3aeca136
Ivan Moret <ivan.moret@gmail.com>
2024-09-25 22:10:33 +0200

    Quitando lo que sobra

--- landuino_MQTT/build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/moret/nextcloud/documentos/Solar/consumoExcedentes/sketchs/landuino_MQTT/build/core/WString.cpp.o: \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.cpp \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/WString.h
