73a9db3a1602c632d9faf9f379bc0d8b8142bf8f
Ivan Moret <ivan.moret@gmail.com>
2024-09-25 22:08:02 +0200

    Delete landuino_MQTT/build directory

--- landuino_MQTT/build/core/wiring_pulse.S.d
+++
@@ -1,2 +0,0 @@
-/home/moret/nextcloud/documentos/Solar/consumoExcedentes/sketchs/landuino_MQTT/build/core/wiring_pulse.S.o: \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_pulse.S
--- landuino_MQTT/build/core/wiring_shift.c.d
+++
@@ -1,6 +0,0 @@
-/home/moret/nextcloud/documentos/Solar/consumoExcedentes/sketchs/landuino_MQTT/build/core/wiring_shift.c.o: \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_shift.c \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/wiring_private.h \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
- /home/moret/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/standard/pins_arduino.h
