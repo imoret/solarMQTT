2dd7e1b43d8303a507d67467dcc528289de48426
Iván Moret <ivan.moret@gmail.com>
2026-07-08 19:34:42 +0200

    Version funcional

--- web/solarMQTT/settings.py
+++ web/solarMQTT/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','192.168.2.105','solar.uxmor.com']
+CSRF_TRUSTED_ORIGINS = ['http://192.168.2.25:8000', 'http://127.0.0.1:8000', 'http://192.168.2.105:8000', 'http://solar.uxmor.com:8000']
 
 
 # Application definition
